71694b4468e205e98e40dab456d4c50155c65aeb
alinabodnar1 <malina.bodnar@gmail.com>
2023-07-25 21:23:30 +0300

    add camera

--- src/Screens/CreatePostsScreen/CreatePostsScreen.jsx
+++ src/Screens/CreatePostsScreen/CreatePostsScreen.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
 import { EvilIcons, AntDesign } from "@expo/vector-icons";
 import {
   View,
@@ -9,11 +11,28 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Pressable,
+  TouchableOpacity,
   TextInput,
 } from "react-native";
 import { commonStyles } from "../../commonStyles";
 
 export default function CreatePostsScreen() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraRef, setCameraRef] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      await MediaLibrary.requestPermissionsAsync();
+
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === false) {
+    return <Text> No access to camera</Text>;
+  }
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -22,10 +41,44 @@ export default function CreatePostsScreen() {
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={{ flex: 1 }}>
           <View style={commonStyles.wrapper}>
-            <Image
+            {/* <Image
               style={styles.emptyPhoto}
               source={require("./img/empty-photo.jpg")}
-            />
+            /> */}
+            <Camera style={styles.emptyPhoto} type={type} ref={setCameraRef}>
+              <View style={styles.photoView}>
+                <TouchableOpacity
+                  style={styles.flipContainer}
+                  onPress={() => {
+                    setType(
+                      type === Camera.Constants.Type.back
+                        ? Camera.Constants.Type.front
+                        : Camera.Constants.Type.back
+                    );
+                  }}
+                >
+                  <Text
+                    style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+                  >
+                    {" "}
+                    Flip{" "}
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={async () => {
+                    if (cameraRef) {
+                      const { uri } = await cameraRef.takePictureAsync();
+                      await MediaLibrary.createAssetAsync(uri);
+                    }
+                  }}
+                >
+                  <View style={styles.takePhotoOut}>
+                    <View style={styles.takePhotoInner}></View>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </Camera>
             <Pressable>
               <Text style={styles.loadPhoto}>Завантажте фото</Text>
             </Pressable>
@@ -62,6 +115,38 @@ const styles = StyleSheet.create({
     width: 343,
     height: 240,
     borderRadius: 8,
+  },
+  photoView: {
+    flex: 1,
+    backgroundColor: "transparent",
+    justifyContent: "flex-end",
+  },
+
+  flipContainer: {
+    flex: 0.1,
+    alignSelf: "flex-end",
+  },
+
+  button: { alignSelf: "center" },
+
+  takePhotoOut: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 50,
+    width: 50,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 50,
+  },
+
+  takePhotoInner: {
+    borderWidth: 2,
+    borderColor: "white",
+    height: 40,
+    width: 40,
+    backgroundColor: "white",
+    borderRadius: 50,
   },
   loadPhoto: {
     ...commonStyles.font,
