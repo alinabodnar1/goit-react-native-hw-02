bcf3970b9e49aa94b6db08e3fcd8961f10f59990
alinabodnar1 <malina.bodnar@gmail.com>
2023-08-21 13:29:37 +0300

    change store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -10,29 +10,23 @@ import {
   REGISTER,
 } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
 import { postsReducer } from "../redux/posts/postsSlice";
-
-const persistConfig = {
-  key: "root",
-  storage: AsyncStorage,
-};
-
-
-
-const store = configureStore({
+export const store = configureStore({
   reducer: {
-    
     posts: postsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        ignoredActions: [
+          FLUSH,
+          REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER,
+        ],
       },
     }),
 });
-
-const persistor = persistStore(store);
-
-export default { store, persistor };
+export const persistor = persistStore(store);
